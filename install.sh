125ab4415029f60554cfdaa46faaee927ddf1a0f
myOmikron <accounts@omikron.dev>
2020-10-24 17:36:08 +0200

    Added automatic start to the systemd service

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ python3 -m pip install -r requirements.txt
 cp bbb-rec-starter.service /usr/lib/systemd/system/
 ln -s /usr/lib/systemd/system/bbb-rec-starter.service /etc/systemd/system/multi-user.target.wants/
 systemctl daemon-reload
+systemctl enable bbb-rec-starter
 
 cp -r bbb_rec_starter /home/bbb-rec-starter/
 chown -R bbb-rec-starter:bbb-rec-starter /home/bbb-rec-starter/bbb_rec_starter
