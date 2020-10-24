39cd21bc79c2d031d0e5adb103903870f10b340b
myOmikron <accounts@omikron.dev>
2020-10-24 13:45:38 +0200

    Corrected view and urls

--- bbb_rec_starter/api/urls.py
+++ bbb_rec_starter/api/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 
 from api.views import *
 
-urlpatterns = {
-
-}
+urlpatterns = [
+    path('startRecording', StartRecordingView.as_view())
+]
--- bbb_rec_starter/api/views.py
+++ bbb_rec_starter/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+
+class StartRecordingView(View):
+
+    def post(self, request):
+        data = {}
+        return JsonResponse(data)
