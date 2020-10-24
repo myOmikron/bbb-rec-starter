71643b60af3db5b90d5e77a3cd17c916cef544dd
myOmikron <accounts@omikron.dev>
2020-10-24 17:33:06 +0200

    Added installer

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+adduser bbb-rec-starter
+apt install python3 python3-pip chromium
+python3 -m pip install -r requirements.txt
+
+cp bbb-rec-starter.service /usr/lib/systemd/system/
+ln -s /usr/lib/systemd/system/bbb-rec-starter.service /etc/systemd/system/multi-user.target.wants/
+systemctl daemon-reload
+
+cp -r bbb_rec_starter /home/bbb-rec-starter/
+chown -R bbb-rec-starter:bbb-rec-starter /home/bbb-rec-starter/bbb_rec_starter
+
